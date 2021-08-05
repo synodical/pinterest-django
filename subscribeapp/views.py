19b1f719404a19d3cd420df04b29d0eb33eff7d8
synodical <lena0323park@naver.com>
2021-08-05 19:02:52 +0900

    django course 43

--- subscribeapp/views.py
+++ subscribeapp/views.py
@@ -4,8 +4,9 @@ from django.shortcuts import render, get_object_or_404
 # Create your views here.
 from django.urls import reverse
 from django.utils.decorators import method_decorator
-from django.views.generic import RedirectView
+from django.views.generic import RedirectView, ListView
 
+from articleapp.models import Article
 from projectapp.models import Project
 from subscribeapp.models import Subscription
 
@@ -19,12 +20,23 @@ class SubscriptionView(RedirectView):
     def get(self, request, *args, **kwargs):
         project = get_object_or_404(Project, pk=self.request.GET.get('project_pk'))
         user = self.request.user
-
         subscription = Subscription.objects.filter(user=user,
                                                   project=project)
         if subscription.exists():
             subscription.delete()
         else:
             Subscription(user=user, project=project).save()
-
         return super(SubscriptionView, self).get(request, *args, **kwargs)
+
+@method_decorator(login_required, 'get')
+class SubscriptionListView(ListView):
+    model = Article
+    context_object_name = 'article_list'
+    template_name = 'subscribeapp/list.html'
+    pagenate_by = 5
+
+    #가지고 오는 게시글의 조건을 정해줍니다.
+    def get_queryset(self):
+        projects = Subscription.objects.filter(user=self.request.user).values_list('project')
+        article_list = Article.objects.filter(project__in=projects)
+        return article_list
